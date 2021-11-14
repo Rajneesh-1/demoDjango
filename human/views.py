0303f7421c8a506636ae2be5c7f2ecfff8f91b7c
RAJNEESH <rajneeshk.sharma@skilrock.com>
2021-11-14 08:18:02 +0530

    Simple Django Project without Database codes

---
+++ human/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+
+    return render(request, 'homeHTML/home.html')
+
+def login(request):
+
+    x = ["RAJNEESH", "Monu"]
+    return render(request, 'loginHTML/login.html', {"Data": x})
