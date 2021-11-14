880132235268390d71209852cc6d2c26dd9576ac
RAJNEESH <rajneeshk.sharma@skilrock.com>
2021-11-14 16:49:56 +0530

    Now database codes are available in it

--- human/models.py
+++ human/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class DetailsOfHuman(models.Model):
+    name = models.CharField(max_length=15)
+    city = models.CharField(max_length=10)
+    favourite_food = models.CharField(max_length=15)
--- human/views.py
+++ human/views.py
@@ -1,11 +1,28 @@
-from django.shortcuts import render
+import requests
+from django.shortcuts import render,HttpResponseRedirect
+from human.models import DetailsOfHuman
 
 # Create your views here.
 def home(request):
 
-    return render(request, 'homeHTML/home.html')
+    if request.method == 'POST':
+        name = request.POST['name']
+        city = request.POST['city']
+        favourite_food = request.POST['favourite_food']
+
+        recieved_data = []
+        recieved_data.append(name)
+        recieved_data.append(city)
+        recieved_data.append(favourite_food)
+
+        instanceOfDetails = DetailsOfHuman.objects.create(name=name, city=city, favourite_food=favourite_food)
+        instanceOfDetails.save()
+
+    getSavedDetails = DetailsOfHuman.objects.all()
+    print("getSavedDetails", getSavedDetails)
+
+    return render(request, 'homeHTML/home.html',{"getSavedDetails": getSavedDetails})
 
 def login(request):
 
-    x = ["RAJNEESH", "Monu"]
-    return render(request, 'loginHTML/login.html', {"Data": x})
+    return render(request, 'loginHTML/login.html')
